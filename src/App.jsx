f243db1e88d7834545e85fa5c8389a146663e128
ruifortescv <ruimiguelfortes@gmail.com>
2025-11-20 01:27:08 +0000

    upgrade pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CarList from './components/CarList'
 import { Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import FiltroVeiculo from './components/FiltroVeiculo'
 import HomeHead from './components/HomeHead'
 
@@ -27,6 +28,20 @@ export default function App() {
           </>
           } 
         />
+        <Route   path="/pesquisa" element={
+          <>  
+              <HomeHead/> 
+              <HomeContent/>
+
+              
+          </>
+          } 
+        />  
+       <Route Link to="/inicio" element= {<div>
+              <HomeHead/> 
+              <FiltroVeiculo/>
+              <CarList /> 
+           </div>}/>
       </Routes>
     </div>
   )
--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,24 +1,59 @@
-import React from 'react'
+import React , {useState} from 'react'
 import useFetch from '../hooks/useFetch'
 import { fetchCars } from '../services/api'
 import CarCard from './CarCard'
 import DivEspaco from './DivEspaco'
 import '../styles/app.css'
+import { Link} from 'react-router-dom'
+
 import logo from '/racv.png'
 import uta from '/uta.png'
+export function HomeContent() {
+  const [locais, setLocais] = useState("");
+  const [veiculo, setVeiculo] = useState("");
 
-export function HomeContent(){
-  return(
-         
-        <div className='body-main'>
-                <input className="filtro-geral" placeholder='Veiculos' type='text' name='veiculo' ></input>
-        <label for="desc"></label>
-        
-        <input className="fitro-locais-geral" placeholder='Locais' type='text' name='veiculo'></input>
-        
-        <button className='butao'>Pesquisar</button>
-      
-        </div>
-  )
-    
+  function RequestServidor(e) {
+    e.preventDefault(); // evita reload da página
+    console.log("Veículo:", veiculo);
+    console.log("Locais:", locais);
+    // Aqui você chamaria a função que faz a requisição ao servidor
+    // ex: fetchCars({ veiculo, locais })
+      useEffect(() => {
+      async function fetchCarros() {
+        try {
+          const res = await fetch("http://127.0.0.1:5000/api/carros");
+          const data = await res.json();
+          setCarros(data);
+        } catch (error) {
+          console.error("Erro:", error);
+        }
+      }
+      fetchCarros();
+    }, []);
+  }
+
+  return (
+    <div className='body-main'>
+      <form onSubmit={RequestServidor}>
+        <input
+          className="filtro-geral"
+          placeholder='Veículos'
+          value={veiculo}
+          type='text'
+          name='veiculo'
+          onChange={(e) => setVeiculo(e.target.value)}
+        />
+        <input
+          className="fitro-locais-geral"
+          placeholder="Locais"
+          value={locais}
+          type='text'
+          name='locais'
+          onChange={(e) => setLocais(e.target.value)}
+        />
+        <button className='butao' type='submit'>Pesquisar</button>
+      </form>
+      <Link to="/">About</Link>
+    </div>
+  );
 }
